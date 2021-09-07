25d0dfbafcdd17acaa7ba990224154a28938b085
JWKorte <jasper.korte@gmx.de>
2021-09-07 13:19:34 +0200

    probe

--- train.py
+++ train.py
@@ -25,5 +25,5 @@ regressor = regressor.fit(x_train, y_train)
 
 y_pred = regressor.predict(x_test) 
 
-file_to_write = open("models/baummethoden.pickle", "wb")
+file_to_write = open("baummethoden.pickle", "wb")
 pickle.dump(regressor, file_to_write)
